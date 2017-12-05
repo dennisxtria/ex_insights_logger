35793f59e77bfafa5496a9b82a55fd5ac26cb85b
Konstantinos Stathopoulos <k.stathopoulos@stoiximan.gr>
2017-12-05 14:34:57 +0200

    test(app): improved tests and did some refactoring

--- lib/ex_insights_logger.ex
+++ lib/ex_insights_logger.ex
@@ -7,11 +7,11 @@ defmodule ExInsightsLogger do
 
   def handle_event({level, _gl, {_Logger, msg, timestamp, metadata}}, state) do
     event = %{timestamp: inspect(timestamp), message: msg, metadata: inspect(metadata)}
-
+    #IO.inspect metadata[:value]
+    
     case level do
-      :info -> ExInsights.track_trace(msg, level, %{"metadata" => inspect(metadata)})
-      :debug -> ExInsights.track_event(msg, %{"metadata" => inspect(metadata)})
-      :warn -> ExInsights.track_trace(msg, level, %{"metadata" => inspect(metadata)})
+      lvl when lvl in [:info, :warn] -> ExInsights.track_trace(msg, level, %{"metadata" => inspect(metadata)})
+      :debug -> ExInsights.track_metric(msg, metadata[:value], %{"metadata" => inspect(metadata)})
       :error -> ExInsights.track_exception(msg, :erlang.get_stacktrace, nil, %{"metadata" => inspect(metadata)})
     end  
     {:ok, state}
